ceecbb656d28864066c8d83029605579cb9d3ac0
SaiRohan Velpula <7690rohan@gmail.com>
2025-06-07 21:12:08 +0530

    created one more data 4

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,37 +1,45 @@
 class Node {
     int data;
-    Node left,right;
+    Node left, right;
 
-    Node(int data){
-        this.data=data;
-        this.left=null;
-        this.right=null;
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-    int getData(){
+
+    int getData() {
         return data;
     }
-    void setData(int data){
-        this.data=data;
+
+    void setData(int data) {
+        this.data = data;
     }
-    Node getLeft(){
+
+    Node getLeft() {
         return left;
     }
-    void setLeft(Node left){
-        this.left=left;
+
+    void setLeft(Node left) {
+        this.left = left;
     }
-    Node getRight(){
+
+    Node getRight() {
         return right;
     }
-    void setRight(Node right){
-        this.right=right;
+
+    void setRight(Node right) {
+        this.right = right;
     }
 }
+
 public class BinaryTree {
- 
+
     public static void main(String[] args) {
-        Node obj1=new Node(1);
+        Node obj1 = new Node(1);
         obj1.setLeft(new Node(2));
         obj1.setRight(new Node(3));
+        obj1.setLeft(new Node(4));
     }
-    
+
 }
